d4055810625ff382883632451324b3f652991f4e
Lyle Christine <cxz@gmx.com>
2019-12-22 11:16:59 +0000

    styling added

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 start_spring_boot() {
-  echo "**Spring Boot running**"
+  echo "start_spring_boot() running"
+  cd ../ &&
+  chmod +x ./mvnw &&
   ./mvnw spring-boot:run
 }
 
